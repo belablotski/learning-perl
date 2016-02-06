8608f93074b9315bbf9b6498272e58d458f44839
Aliaksei Belablotski <v-abelablotski@expedia.com>
2016-02-05 19:57:22 -0800

    Perl Arrays and Hashes

---
+++ 03-array.pl
@@ -0,0 +1,4 @@
+my @words = ("hello", "perl", "world");
+my @numbers = (1, 2, 3, 5, 7, 11, 13, 17);
+my @mix   = ("test", 123, 4.56);
+
---
+++ 04-hash.pl
@@ -0,0 +1,5 @@
+# Hash is a map (associative array)
+
+my %num1 = (0, "zero", 1, "one", 2, "two");
+
+my %num2 = (0 => "zero", 1 => "one", 2 => "two");
